62e3dc6896fbfcd5102340d85b8a7ce9d5b5ef78
CJ <cjfritz9@gmail.com>
2023-04-26 10:37:38 -0600

    changed dog treat roll logic

--- dist/utils/joewatermelon.js
+++ dist/utils/joewatermelon.js
@@ -1,13 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const getDogTreat = () => {
-    const roll = Math.round(Math.random() * 2);
-    if (roll === 0) {
+    const roll = Math.round(Math.random() * 99);
+    if (roll < 33) {
         return 'Finn is a good pupper and gets a treat! joewatFinn';
     }
-    if (roll === 1) {
+    else if (roll < 66) {
         return 'Tilly is a good pupper and gets a treat! joewatTilly';
     }
-    return 'Zippy is a good pupper and gets a treat! joewatZippy';
+    else {
+        return 'Zippy is a good pupper and gets a treat! joewatZippy';
+    }
 };
 exports.default = getDogTreat;
--- src/utils/joewatermelon.ts
+++ src/utils/joewatermelon.ts
@@ -1,13 +1,13 @@
 const getDogTreat = () => {
-  const roll = Math.round(Math.random() * 2);
+  const roll = Math.round(Math.random() * 99);
 
-  if (roll === 0) {
+  if (roll < 33) {
     return 'Finn is a good pupper and gets a treat! joewatFinn';
-  }
-  if (roll === 1) {
+  } else if (roll < 66) {
     return 'Tilly is a good pupper and gets a treat! joewatTilly';
+  } else {
+    return 'Zippy is a good pupper and gets a treat! joewatZippy';
   }
-  return 'Zippy is a good pupper and gets a treat! joewatZippy';
 };
 
 export default getDogTreat;
