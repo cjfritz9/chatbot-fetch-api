2f0ca7f186ba2f2945a07f3e01eb261c6d771681
CJ <cjfritz9@gmail.com>
2023-04-26 11:06:43 -0600

    changed phrasing of dog treat response

--- src/utils/joewatermelon.ts
+++ src/utils/joewatermelon.ts
@@ -2,11 +2,11 @@ const getDogTreat = () => {
   const roll = Math.round(Math.random() * 99);
 
   if (roll < 33) {
-    return 'Finn is a good pupper and gets a treat! joewatFinn';
+    return 'Finn is the good pupper and gets a treat! joewatFinn';
   } else if (roll < 66) {
-    return 'Tilly is a good pupper and gets a treat! joewatTilly';
+    return 'Tilly is the good pupper and gets a treat! joewatTilly';
   } else {
-    return 'Zippy is a good pupper and gets a treat! joewatZippy';
+    return 'Zippy is the good pupper and gets a treat! joewatZippy';
   }
 };
 
