71579553cab452c8402008af868600abc50548b0
CJ <cjfritz9@gmail.com>
2023-04-27 21:28:15 -0600

    added get responses to new route

--- src/routes/joewatermelon.ts
+++ src/routes/joewatermelon.ts
@@ -10,17 +10,22 @@ joeRouter.get('/auth', (req, res) => {
   console.log('request original url: ', req.originalUrl);
   console.log('request body: ', req.body);
   console.log('doc hash: ', document.location.hash);
-  console.log('req hash: ', document.location.hash);
   res.send('Success');
 });
 
-joeRouter.get('/token', async (_req, _res) => {
+joeRouter.get('/token', async (_req, res) => {
   const response = await axios.post(
     'https://id.twitch.tv/oauth2/token',
     "client_id=1xkvdpm0d3i7kkfsvcglm29dvv71g4&client_secret=cme6w5kqh6b0bzvm7ag45ypfdxoibl&code=r1aikt0hk9fb7fuo7z0d8ppmdowb03&grant_type=authorization_code&redirect_uri=https://nightbot-fetch-api-l75xpo5a3a-uc.a.run.app/joewatermelon/auth"
   );
   console.log('full res: ', response)
   console.log('res data: ', response.data)
+  if (response && response.data) {
+    res.send(response.data)
+
+  } else {
+    res.send('Failed')
+  }
 });
 
 joeRouter.get('/dog_treat', (_req, res) => {
