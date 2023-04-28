0bccfa6b58040315716fa202b70dd26d98492ad5
CJ <cjfritz9@gmail.com>
2023-04-27 20:43:09 -0600

    fixed typo

--- src/routes/joewatermelon.ts
+++ src/routes/joewatermelon.ts
@@ -7,6 +7,7 @@ joeRouter.get('/auth', (req, res) => {
   console.log('full request: ', req);
   console.log('request url: ', req.url);
   console.log('request original url: ', req.originalUrl);
+  console.log('request body: ', req.body);
   res.send('Success')
 });
 
@@ -16,4 +17,4 @@ joeRouter.get('/dog_treat', (_req, res) => {
 
 export default joeRouter;
 
-('https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=1xkvdpm0d3i7kkfsvcglm29dvv71g4&redirect_uri=http://localhost:3000&scope=channel%3Amanage%3Apolls+channel%3Aread%3Apolls&state=c3ab8aa609ea11e793ae92361f002671');
+console.log('https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=1xkvdpm0d3i7kkfsvcglm29dvv71g4&redirect_uri=http://localhost:3000&scope=channel%3Amanage%3Apolls+channel%3Aread%3Apolls&state=c3ab8aa609ea11e793ae92361f002671');
