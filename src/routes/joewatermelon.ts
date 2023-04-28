6e91fdd658982a56d460c4bd07d0f10431110d94
CJ <cjfritz9@gmail.com>
2023-04-27 20:47:55 -0600

    testing auto redeployment

--- src/routes/joewatermelon.ts
+++ src/routes/joewatermelon.ts
@@ -8,13 +8,15 @@ joeRouter.get('/auth', (req, res) => {
   console.log('request url: ', req.url);
   console.log('request original url: ', req.originalUrl);
   console.log('request body: ', req.body);
-  res.send('Success')
+  res.send('Success');
 });
 
 joeRouter.get('/dog_treat', (_req, res) => {
-  res.send(getDogTreat());
+  res.send('test' + getDogTreat());
 });
 
 export default joeRouter;
 
-console.log('https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=1xkvdpm0d3i7kkfsvcglm29dvv71g4&redirect_uri=http://localhost:3000&scope=channel%3Amanage%3Apolls+channel%3Aread%3Apolls&state=c3ab8aa609ea11e793ae92361f002671');
+console.log(
+  'https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=1xkvdpm0d3i7kkfsvcglm29dvv71g4&redirect_uri=http://localhost:3000&scope=channel%3Amanage%3Apolls+channel%3Aread%3Apolls&state=c3ab8aa609ea11e793ae92361f002671'
+);
