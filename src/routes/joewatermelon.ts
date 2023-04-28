f9187ef9b438ea56354293a02a326dc4e89bc794
CJ <cjfritz9@gmail.com>
2023-04-27 20:25:55 -0600

    added auth fetch endpoint for joewatermelon

--- src/routes/joewatermelon.ts
+++ src/routes/joewatermelon.ts
@@ -3,6 +3,12 @@ import getDogTreat from '../utils/joewatermelon';
 
 const joeRouter = express.Router();
 
+joeRouter.get('/auth', (req, _res) => {
+  console.log('full request: ', req)
+  console.log('request url: ', req.url)
+  console.log('request original url: ', req.originalUrl)
+});
+
 joeRouter.get('/dog_treat', (_req, res) => {
   res.send(getDogTreat());
 });
