f80a256f787db17eca037d90f21755f92761eeae
CJ <cjfritz9@gmail.com>
2023-04-27 20:38:54 -0600

    added a test response for joewatermelon/auth

--- src/routes/joewatermelon.ts
+++ src/routes/joewatermelon.ts
@@ -3,10 +3,11 @@ import getDogTreat from '../utils/joewatermelon';
 
 const joeRouter = express.Router();
 
-joeRouter.get('/auth', (req, _res) => {
-  console.log('full request: ', req)
-  console.log('request url: ', req.url)
-  console.log('request original url: ', req.originalUrl)
+joeRouter.get('/auth', (req, res) => {
+  console.log('full request: ', req);
+  console.log('request url: ', req.url);
+  console.log('request original url: ', req.originalUrl);
+  res.send('Success')
 });
 
 joeRouter.get('/dog_treat', (_req, res) => {
@@ -14,3 +15,5 @@ joeRouter.get('/dog_treat', (_req, res) => {
 });
 
 export default joeRouter;
+
+('https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=1xkvdpm0d3i7kkfsvcglm29dvv71g4&redirect_uri=http://localhost:3000&scope=channel%3Amanage%3Apolls+channel%3Aread%3Apolls&state=c3ab8aa609ea11e793ae92361f002671');
