10e965a22c76c2edf8bbc86726e5a8b826b5277e
CJ <cjfritz9@gmail.com>
2023-05-02 21:58:25 -0600

    slightly changed logic for dog treat roll making it more even

--- dist/utils/joewatermelon/index.js
+++ dist/utils/joewatermelon/index.js
@@ -1,18 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const getDogTreat = () => {
-    const roll = Math.round(Math.random() * 100);
+    const roll = Math.round(Math.random() * 99);
     if (roll < 33) {
         return 'Finn is the good pupper and gets a treat! joewatFinn';
     }
     else if (roll < 66) {
         return 'Tilly is the good pupper and gets a treat! joewatTilly';
     }
-    else if (roll < 100) {
+    else if (roll < 99) {
         return 'Zippy is the good pupper and gets a treat! joewatZippy';
     }
     else {
-        return 'Joe is the good boy and gets a pat PATTHEMELON';
+        return 'Joe is the good boy and gets a pat PATTHEMELON (1/100)';
     }
 };
 exports.default = getDogTreat;
--- src/utils/joewatermelon/index.ts
+++ src/utils/joewatermelon/index.ts
@@ -1,14 +1,14 @@
 const getDogTreat = () => {
-  const roll = Math.round(Math.random() * 100);
+  const roll = Math.round(Math.random() * 99);
 
   if (roll < 33) {
     return 'Finn is the good pupper and gets a treat! joewatFinn';
   } else if (roll < 66) {
     return 'Tilly is the good pupper and gets a treat! joewatTilly';
-  } else if (roll < 100) {
+  } else if (roll < 99) {
     return 'Zippy is the good pupper and gets a treat! joewatZippy';
   } else {
-    return 'Joe is the good boy and gets a pat PATTHEMELON'
+    return 'Joe is the good boy and gets a pat PATTHEMELON (1/100)'
   }
 };
 
