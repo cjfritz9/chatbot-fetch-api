aed027e3c37d66e314a1ae2e1aeb48597fac024c
CJ <cjfritz9@gmail.com>
2024-01-18 19:40:17 -0500

    added error handling to last route

--- dist/utils/joewatermelon/youtube.js
+++ dist/utils/joewatermelon/youtube.js
@@ -16,7 +16,15 @@ exports.getLatestYtMedia = void 0;
 const axios_1 = __importDefault(require("axios"));
 const getLatestYtMedia = () => __awaiter(void 0, void 0, void 0, function* () {
     const response = yield axios_1.default.get('https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC-vjqhlVcyT54gUgzCGO13A&maxResults=1&order=date&type=video&key=AIzaSyD2sRkhratG3GHXwi220t7b47bLEXvO5pc');
-    const media = response.data.items[0];
-    return `Check out my latest video! ${media.snippet.title} https://www.youtube.com/watch?v=${media.id.videoId}`;
+    if (response &&
+        response.data &&
+        response.data.items &&
+        response.data.items[0]) {
+        const media = response.data.items[0];
+        return `Check out my latest video! ${media.snippet.title} https://www.youtube.com/watch?v=${media.id.videoId}`;
+    }
+    else {
+        return 'Error fetching latest video.';
+    }
 });
 exports.getLatestYtMedia = getLatestYtMedia;
--- src/utils/joewatermelon/youtube.ts
+++ src/utils/joewatermelon/youtube.ts
@@ -5,7 +5,16 @@ export const getLatestYtMedia = async () => {
     'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC-vjqhlVcyT54gUgzCGO13A&maxResults=1&order=date&type=video&key=AIzaSyD2sRkhratG3GHXwi220t7b47bLEXvO5pc'
   );
 
-  const media = response.data.items[0];
+  if (
+    response &&
+    response.data &&
+    response.data.items &&
+    response.data.items[0]
+  ) {
+    const media = response.data.items[0];
 
-  return `Check out my latest video! ${media.snippet.title} https://www.youtube.com/watch?v=${media.id.videoId}`;
+    return `Check out my latest video! ${media.snippet.title} https://www.youtube.com/watch?v=${media.id.videoId}`;
+  } else {
+    return 'Error fetching latest video.'
+  }
 };
